4515cc9c21df89f7c206d9bd7492e91e3e1fbb3f
gyi8787 <gyi89052@gmail.com>
2023-06-29 19:13:44 +0530

    making folder

---
+++ javasript/array.js
@@ -0,0 +1,16 @@
+let a=["hello",1,true];
+console.log(a);
+a.pop(); // remove last element of an array
+console.log(a);
+a.push("'guys")   //ye last me add kerta h
+a.unshift("wlcm")  //ye starting me add kerta h
+a.shift();       // ye starting vala element remove kerta h
+// console.log(a);
+
+//2d array
+
+let arr=[
+    ['1','2','3'],
+    [3,4,5]
+]
+console.log(arr)
