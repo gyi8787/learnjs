92a7fc1659ca55fb95752892d1235917ddabd13a
gyi8787 <gyi89052@gmail.com>
2023-06-30 23:18:02 +0530

    msg

--- akki.js
+++ akki.js
@@ -1,8 +1,11 @@
-var a=10;
-function b()
-{
-    var x=10;
-}
-console.log(window.a);
-console.log(a);
-console.log(this.a)
+// var a=10;
+// function b()
+// {
+//     var x=10;
+// }
+// console.log(window.a);
+// console.log(a);
+// console.log(this.a)
+
+// https://youtu.be/QCRpVw2KXf8
+// SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5
