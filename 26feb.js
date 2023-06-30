8347de0b864a396a1c58b4f046d3e6ec86f72705
gyi8787 <gyi89052@gmail.com>
2023-06-30 17:17:51 +0530

    practice.js

--- 26feb.js
+++ 26feb.js
@@ -68,3 +68,16 @@ changeageandname();
 console.log(person);
 // =====================================================
 //bahut sare question practice kerne ki jaroorat hai
+// typeof operator ek string return kerta hai
+//  falsy-false, 0, -0, 0n, "", null, undefined, and NaN.
+// == value vs === datatype+value
+
+var user={
+    email:"my@gmail.com",
+    upadateEmail: function(email)
+    {
+        this.email=email;
+    },
+};
+user.upadateEmail("new@gmail");
+console.log(user.email);
