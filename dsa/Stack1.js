f05d861f69f08380cf2fa1e0b1d149dba0adfe83
gyi8787 <gyi89052@gmail.com>
2023-07-05 00:57:56 +0530

    msg

--- dsa/Stack1.js
+++ dsa/Stack1.js
@@ -58,3 +58,63 @@ public static void main(String[] args)
             System.out.println(true);
         }
     }
+ ===============>function ka use ker liye<============
+
+
+ public static boolean check( Stack<Character>st,char bracket)
+ {
+ if(st.size()==0)
+     return false;
+ else if (st.peek()!=bracket)
+ {
+     return false;
+ }
+ else
+ {
+     st.pop();
+     return true;
+ }
+ }
+ public static void main(String[] args)
+ {
+     String exp="[(a+b)+{(c+d)*(e/f)}]";
+     Stack<Character>st=new Stack<>();
+     for(int i=0;i<exp.length();i++)
+     {
+         char ch=exp.charAt(i);
+         if(ch=='('||ch=='{'||ch=='[')
+         {
+             st.push(ch);
+         }
+         else if (ch==')')
+         {
+             boolean ans=check(st,'(');
+             if(ans==false){
+                 System.out.println(false);
+                 return;
+             }
+         }
+         else if (ch=='}')
+         {
+             boolean ans=check(st,'{');
+             if(ans==false){
+                 System.out.println(false);
+                 return;
+             }
+         }
+         else if (ch==']')
+         {
+             boolean ans=check(st,'[');
+             if(ans==false){
+                 System.out.println(false);
+                 return;
+             }
+         }
+     }
+     if(st.size()!=0){
+         System.out.println(false);
+     }
+     else{
+         System.out.println(true);
+     }
+ }
