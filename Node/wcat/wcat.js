5756ae2f8e2ae39d978b6e9ad0cac8612cc73d94
gyi8787 <gyi89052@gmail.com>
2023-07-04 18:24:20 +0530

    split in wcat

--- Node/wcat/wcat.js
+++ Node/wcat/wcat.js
@@ -2,6 +2,7 @@
 // wcat ke liye likho node wcat.js msg
 //1) node wcat.js filepath => displays the contents of a file in terminal
 //2) node wcat.js filepath1 filepath2 filepath3 => displays the contents of a file in terminal in concatation in given order
+//3) node wcat.js -n file1 file2 file3 OR node wcat.js -n file1 => displays the contents of a file in terminal sath me number bhi aayenge
 // node wcat.js f1.txt
 //node wcat.js f1.txt f2.txt f3.txt
 
@@ -12,15 +13,27 @@
 //   'helllo]
 // let input=process.argv.slice(2);// jb slice lagate tb uper vali do line chali jati h
 // console.log(input);
-// =============================================================
+// ===============================================================================
 const fs=require("fs");
 let inputArr=process.argv.slice(2);
+console.log(inputArr);
 let filesArr=[];  //placed files path in filesArr
+let optionsArr=[];
+//===================>placed files path in fileArr <===============================
 for(let i=0;i<inputArr.length;i++)
 {
-    filesArr.push(inputArr[i]);
+    let firstChar = inputArr[i].charAt(0);
+    // console.log(firstChar);
+    if(firstChar=='-')
+    {
+        optionsArr.push(inputArr[i]);
+    }
+    else{
+        filesArr.push(inputArr[i]);
+    }
 }
 console.log("file to be read are"+filesArr);
+//===================================>check if all the files are present<============//
 // node wcat.js filepath1 filepath2 filepath3 
 // check if all the files are present
 for(let i=0;i<filesArr.length;i++)
@@ -32,6 +45,7 @@ for(let i=0;i<filesArr.length;i++)
         return;
     }
 }
+//===================================>content read and appending starts <=======================//
 //content read and appending starts
 let content="";
 for(let i=0;i<filesArr.length;i++)
@@ -40,3 +54,8 @@ for(let i=0;i<filesArr.length;i++)
     content+=fileContent+"\n"; // jo bhi content aata jayega wo isme append hota jayega
 }
 console.log(content);
+let contentArr=content.split("\n"); 
+// =================================================================
+// const fs=require("fs");
+// let inputArr=process.argv.slice(2);
+// console.log(inputArr);  //node wcat.js -n f1.txt
